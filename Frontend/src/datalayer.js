fc23f14bfb1d114d53851a05287719ab55f7a18d
Reid Compton <reid@reidcompton.com>
2018-11-10 12:59:02 -0600

    change serviceurl in datalayer

--- Frontend/src/datalayer.js
+++ Frontend/src/datalayer.js
@@ -1,5 +1,5 @@
 const datalayer = {
-    serviceUrl: 'https://localhost:44330/api/home',
+    serviceUrl: 'https://improvsax20181110122842.azurewebsites.net/api/home',
     get (start, song) {
       const response = fetch(this.serviceUrl)
       return response
